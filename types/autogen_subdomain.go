14164dc243760f4bf63c39754c71868a6c72770d
Scott Sickles <scottsickles@gmail.com>
2021-04-27 11:52:17 -0400

    small update

--- types/autogen_subdomain.go
+++ types/autogen_subdomain.go
@@ -2,10 +2,12 @@ package types
 
 type AutogenSubdomain struct {
 	IdModel
-	OrgName     string   `json:"orgName"`
-	DnsName     string   `json:"dnsName"`
-	DomainName  string   `json:"domainName"`
-	Fqdn		string	 `json:"fqdn"`
-	Certificate string   `json:"certificate"`
-	Nameservers []string `json:"nameservers"`
+	OrgName     string      `json:"orgName"`
+	DnsName     string      `json:"dnsName"`
+	DomainName  string      `json:"domainName"`
+	Fqdn        string      `json:"fqdn"`
+	Certificate string      `json:"certificate"`
+	Nameservers Nameservers `json:"nameservers"`
 }
+
+type Nameservers []string
