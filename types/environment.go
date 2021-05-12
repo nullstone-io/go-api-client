fabc36c7b5cc5f17e82cc69b04e416eb2f48549e
Brad Sickles <brad.sickles@gmail.com>
2021-05-12 11:22:03 -0400

    Non-pointer provider config

--- types/environment.go
+++ types/environment.go
@@ -2,9 +2,9 @@ package types
 
 type Environment struct {
 	IdModel
-	Name           string          `json:"name"`
-	OrgName        string          `json:"orgName"`
-	StackName      string          `json:"stackName"`
-	PipelineOrder  int             `json:"pipelineOrder"`
-	ProviderConfig *ProviderConfig `json:"providerConfig"`
+	Name           string         `json:"name"`
+	OrgName        string         `json:"orgName"`
+	StackName      string         `json:"stackName"`
+	PipelineOrder  int            `json:"pipelineOrder"`
+	ProviderConfig ProviderConfig `json:"providerConfig"`
 }
