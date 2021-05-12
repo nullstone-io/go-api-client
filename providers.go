401206865f02f1a505dd660452162aed9631a036
Brad Sickles <brad.sickles@gmail.com>
2021-05-12 12:08:58 -0400

    Added Providers endpoints

--- providers.go
+++ providers.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 	"net/http"
 	"path"
@@ -40,4 +41,53 @@ func (s Providers) Get(providerName string) (*types.Provider, error) {
 		return nil, err
 	}
 	return &provider, nil
+}
+
+// Create - POST /orgs/:orgName/providers
+func (s Providers) Create(provider *types.Provider) (*types.Provider, error) {
+	rawPayload, _ := json.Marshal(provider)
+	res, err := s.Client.Do(http.MethodPost, path.Join("providers"), nil, nil, json.RawMessage(rawPayload))
+	if err != nil {
+		return nil, err
+	}
+
+	var updatedProvider types.Provider
+	if err := s.Client.ReadJsonResponse(res, &updatedProvider); IsNotFoundError(err) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &updatedProvider, nil
+}
+
+// Update - PUT/PATCH /orgs/:orgName/providers/:name
+func (s Providers) Update(providerName string, provider *types.Provider) (*types.Provider, error) {
+	rawPayload, _ := json.Marshal(provider)
+	endpoint := path.Join("providers", providerName)
+	res, err := s.Client.Do(http.MethodPut, endpoint, nil, nil, json.RawMessage(rawPayload))
+	if err != nil {
+		return nil, err
+	}
+
+	var updatedProvider types.Provider
+	if err := s.Client.ReadJsonResponse(res, &updatedProvider); IsNotFoundError(err) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &updatedProvider, nil
+}
+
+// Destroy - DELETE /orgs/:orgName/providers/:name
+func (s Providers) Destroy(providerName string) (bool, error) {
+	res, err := s.Client.Do(http.MethodDelete, path.Join("providers", providerName), nil, nil, nil)
+	if err != nil {
+		return false, err
+	}
+	if err := s.Client.VerifyResponse(res); IsNotFoundError(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
 }
