474079b147f902f4741d8313fb2c2563192d4f1c
Brad Sickles <brad.sickles@gmail.com>
2021-05-17 17:07:09 -0400

    Add StackId to block and env

--- types/block.go
+++ types/block.go
@@ -4,6 +4,7 @@ type Block struct {
 	IdModel
 	Type                string                     `json:"type"`
 	OrgName             string                     `json:"orgName"`
+	StackId             int64                      `json:"stackId"`
 	StackName           string                     `json:"stackName"`
 	Reference           string                     `json:"reference"`
 	Name                string                     `json:"name"`
