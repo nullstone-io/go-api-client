91c934d7b2511ddc7eff84a6bce8deab19d533c2
Brad Sickles <brad.sickles@gmail.com>
2021-05-18 16:03:45 -0400

    Deprecating ParentBlocks, undoing StackName from block

--- types/block.go
+++ types/block.go
@@ -5,13 +5,13 @@ type Block struct {
 	Type                string                     `json:"type"`
 	OrgName             string                     `json:"orgName"`
 	StackId             int64                      `json:"stackId"`
+	StackName           string                     `json:"stackName"`
 	Reference           string                     `json:"reference"`
 	Name                string                     `json:"name"`
 	ModuleSource        string                     `json:"moduleSource"`
 	ModuleSourceVersion string                     `json:"moduleSourceVersion"`
-	ParentBlocks        map[string]string          `json:"parentBlocks"`
 	Connections         map[string]BlockConnection `json:"connections"`
 
 	// Deprecated
-	StackName           string                     `json:"stackName"`
+	ParentBlocks map[string]string `json:"parentBlocks"`
 }
