fa889f3a8be14e57613da50362525d8872371f57
Brad Sickles <brad.sickles@gmail.com>
2021-05-18 16:07:49 -0400

    Align stacks by id

--- stacks_by_name.go
+++ stacks_by_name.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 	"net/http"
 	"path"
@@ -24,4 +25,22 @@ func (s StacksByName) Get(stackName string) (*types.Stack, error) {
 		return nil, err
 	}
 	return &stack, nil
+}
+
+// Upsert - PUT/PATCH /orgs/:orgName/stacks/:name
+func (s StacksByName) Upsert(stackName string, stack *types.Stack) (*types.Stack, error) {
+	rawPayload, _ := json.Marshal(stack)
+	endpoint := path.Join("stacks", stackName)
+	res, err := s.Client.Do(http.MethodPut, endpoint, nil, nil, json.RawMessage(rawPayload))
+	if err != nil {
+		return nil, err
+	}
+
+	var updatedStack types.Stack
+	if err := s.Client.ReadJsonResponse(res, &updatedStack); IsNotFoundError(err) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &updatedStack, nil
 }
