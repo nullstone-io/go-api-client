f1f93fd5a129486dd1713b952816a5b68e564eca
Brad Sickles <brad.sickles@gmail.com>
2021-05-18 16:07:38 -0400

    Undeprecating env StackName

--- types/environment.go
+++ types/environment.go
@@ -6,9 +6,7 @@ type Environment struct {
 	Reference      string         `json:"reference"`
 	OrgName        string         `json:"orgName"`
 	StackId        int64          `json:"stackId"`
+	StackName      string         `json:"stackName"`
 	ProviderConfig ProviderConfig `json:"providerConfig"`
 	PipelineOrder  int            `json:"pipelineOrder"`
-
-	// Deprecated
-	StackName      string         `json:"stackName"`
 }
