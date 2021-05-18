25254f9aab31bdc118f53ba9ce78fe60778e9236
Brad Sickles <brad.sickles@gmail.com>
2021-05-18 10:51:39 -0400

    Updated blocks with stacks_by_id

--- client.go
+++ client.go
@@ -44,6 +44,10 @@ func (c *Client) Blocks() Blocks {
 	return Blocks{Client: c}
 }
 
+func (c *Client) BlocksByName() BlocksByName {
+	return BlocksByName{Client: c}
+}
+
 func (c *Client) Apps() Apps {
 	return Apps{Client: c}
 }
