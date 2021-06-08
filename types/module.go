f304363d9f007f46f33f4ffe041fc237f7b476de
Scott Sickles <scottsickles@gmail.com>
2021-06-08 17:55:09 -0400

    update module to include the missing ProviderTypes field

--- types/module.go
+++ types/module.go
@@ -1,14 +1,17 @@
 package types
 
+type ProviderTypes []string
+
 type Module struct {
 	UidCreatedModel
-	OrgName     string       `json:"orgName"`
-	Name        string       `json:"name"`
-	Description string       `json:"description"`
-	IsPublic    bool         `json:"isPublic"`
-	Layer       Layer        `json:"layer"`
-	Category    CategoryName `json:"category"`
-	Type        string       `json:"type"`
+	OrgName       string        `json:"orgName"`
+	Name          string        `json:"name"`
+	Description   string        `json:"description"`
+	IsPublic      bool          `json:"isPublic"`
+	Layer         Layer         `json:"layer"`
+	Category      CategoryName  `json:"category"`
+	Type          string        `json:"type"`
+	ProviderTypes ProviderTypes `json:"providerTypes"`
 
 	Versions []ModuleVersion `json:"versions,omitempty"`
 }
