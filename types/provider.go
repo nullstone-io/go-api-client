e0d3ed1b52407b103a9c5e918197eac77ca14205
Brad Sickles <brad.sickles@gmail.com>
2021-07-13 16:35:20 -0400

    Initial draft of gcp credentials in provider

--- types/provider.go
+++ types/provider.go
@@ -6,9 +6,14 @@ import (
 
 type Provider struct {
 	IdModel
-	Name         string          `json:"name"`
-	OrgName      string          `json:"orgName"`
-	ProviderType string          `json:"providerType"`
-	ProviderId   string          `json:"providerId"`
-	Credentials  json.RawMessage `json:"credentials,omitempty"`
+	Name         string `json:"name"`
+	OrgName      string `json:"orgName"`
+	ProviderType string `json:"providerType"`
+
+	// ProviderId represents a single namespace for the provider
+	//   AWS: Account ID
+	//   GCP: Project ID
+	ProviderId string `json:"providerId"`
+
+	Credentials json.RawMessage `json:"credentials,omitempty"`
 }
--- types/provider_config.go
+++ types/provider_config.go
@@ -1,10 +1,17 @@
 package types
 
 type ProviderConfig struct {
-	Aws *AwsProviderConfig `json:"aws"`
+	Aws *AwsProviderConfig `json:"aws,omitempty"`
+	Gcp *GcpProviderConfig `json:"gcp,omitempty"`
 }
 
 type AwsProviderConfig struct {
 	ProviderName string `json:"providerName"`
 	Region       string `json:"region"`
+}
+
+type GcpProviderConfig struct {
+	ProviderName string `json:"providerName"`
+	Region       string `json:"region"`
+	Zone         string `json:"zone"`
 }
