129dddfc316406ab916d6cc82db26ee8ac16ae51
Scott Sickles <scottsickles@gmail.com>
2021-08-06 15:53:01 -0400

    added blockName to match furion output

--- types/connection_target.go
+++ types/connection_target.go
@@ -1,7 +1,8 @@
 package types
 
 type ConnectionTarget struct {
-	StackId int64  `json:"stackId"`
-	BlockId int64  `json:"blockId"`
-	EnvId   *int64 `json:"envId"`
+	StackId   int64  `json:"stackId"`
+	BlockId   int64  `json:"blockId"`
+	BlockName string `json:"blockName"`
+	EnvId     *int64 `json:"envId"`
 }
