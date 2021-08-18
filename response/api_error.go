a2437ed2dabc57137d478801d4a387eac0eca925
Brad Sickles <brad.sickles@gmail.com>
2021-08-18 12:09:35 -0400

    Ensuring we parse the response body for generic api errors

--- response/api_error.go
+++ response/api_error.go
@@ -1,27 +1,32 @@
 package response
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 type ApiError struct {
-	Url        string `json:"url"`
-	Status     int    `json:"status"`
-	StatusText string `json:"statusText"`
-	RequestId  string `json:"requestId"`
+	Url       string `json:"url"`
+	Status    int    `json:"status"`
+	RequestId string `json:"requestId"`
+	Title     string `json:"title"`
+	Type      string `json:"type"`
+	Message   string `json:"message"`
 }
 
 func (e ApiError) Error() string {
-	return fmt.Sprintf("[%s][%s] http error (%d): %s", e.Url, e.RequestId, e.Status, e.StatusText)
+	return fmt.Sprintf("[%s][%s] http error (%d): %s", e.Url, e.RequestId, e.Status, e.Message)
 }
 
 func ApiErrorFromResponse(res *http.Response) ApiError {
-	requestId := res.Header.Get("X-Request-Id")
-	return ApiError{
-		Url:        res.Request.RequestURI,
-		Status:     res.StatusCode,
-		StatusText: res.Status,
-		RequestId:  requestId,
-	}
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+	body := ApiError{}
+	decoder.Decode(&body)
+	// The body doesn't contain url, status code, or request id
+	body.Url = res.Request.RequestURI
+	body.Status = res.StatusCode
+	body.RequestId = res.Header.Get("X-Request-Id")
+	return body
 }
