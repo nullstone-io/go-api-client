d749fc6af199bf46cff17b992238b267d15b79aa
Brad Sickles <brad.sickles@gmail.com>
2021-09-21 20:55:43 -0400

    Added helper to CapabilityConfigs

--- types/capability_configs.go
+++ types/capability_configs.go
@@ -1,3 +1,13 @@
 package types
 
 type CapabilityConfigs []CapabilityConfig
+
+func (s CapabilityConfigs) ExceptNeedsDestroyed() CapabilityConfigs {
+	result := make(CapabilityConfigs, 0)
+	for _, cur := range s {
+		if !cur.NeedsDestroyed {
+			result = append(result, cur)
+		}
+	}
+	return result
+}
