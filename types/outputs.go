caeea080d53c1236b9086f07c411201399a90253
Scott Sickles <scottsickles@gmail.com>
2021-11-02 17:40:26 -0400

    fix OutputItem type

--- types/outputs.go
+++ types/outputs.go
@@ -6,4 +6,5 @@ type OutputItem struct {
 	Type      interface{} `json:"type"` // This is typically a string like "string", "number", but could also be ["tuple", "..."]
 	Value     interface{} `json:"value"`
 	Sensitive bool        `json:"sensitive"`
+	Redacted  bool		  `json:"redacted"`
 }
