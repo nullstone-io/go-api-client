9bf8b9aa89650eccec2f77fe133cc548e5154a8e
Brad Sickles <brad.sickles@gmail.com>
2022-01-14 17:46:13 -0500

    Added public modules endpoint

--- client.go
+++ client.go
@@ -111,6 +111,10 @@ func (c *Client) ModuleVersions() ModuleVersions {
 	return ModuleVersions{Client: c}
 }
 
+func (c *Client) PublicModules() PublicModules {
+	return PublicModules{Client: c}
+}
+
 func (c *Client) Do(method string, relativePath string, query url.Values, headers map[string]string, body interface{}) (*http.Response, error) {
 	var bodyReader io.Reader
 	if jrm, ok := body.(json.RawMessage); ok {
---
+++ public_modules.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"gopkg.in/nullstone-io/go-api-client.v0/response"
+	"gopkg.in/nullstone-io/go-api-client.v0/types"
+	"net/http"
+	"path"
+)
+
+type PublicModules struct {
+	Client *Client
+}
+
+func (m PublicModules) Get(moduleName string) (*types.Module, error) {
+	res, err := m.Client.Do(http.MethodGet, path.Join("public-modules", moduleName), nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var module types.Module
+	if err := m.Client.ReadJsonResponse(res, &module); response.IsNotFoundError(err) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &module, nil
+}
