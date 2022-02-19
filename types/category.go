280552d75a76a12cdb1f04d928a32ef50615a482
Brad Sickles <brad.sickles@gmail.com>
2022-02-19 16:45:46 -0500

    Updated category names

--- types/category.go
+++ types/category.go
@@ -8,12 +8,18 @@ type Category struct {
 type CategoryName string
 
 const (
-	CategoryAppServer     CategoryName = "app/server"
-	CategoryAppContainer  CategoryName = "app/container"
-	CategoryAppServerless CategoryName = "app/serverless"
-	CategoryAppStaticSite CategoryName = "app/static-site"
-	CategorySubdomain     CategoryName = "subdomain"
-	CategoryDomain        CategoryName = "domain"
-	CategoryCapability    CategoryName = "capability"
-	CategoryBlock         CategoryName = "block"
+	CategoryAppContainer          CategoryName = "app/container"
+	CategoryAppServerless         CategoryName = "app/serverless"
+	CategoryAppStaticSite         CategoryName = "app/static-site"
+	CategoryAppServer             CategoryName = "app/server"
+	CategoryCapabilityPublicEntry CategoryName = "capability/public-entry"
+	CategoryCapabilityDatastores  CategoryName = "capability/datastores"
+	CategoryCapabilitySecrets     CategoryName = "capability/secrets"
+	CategoryCapabilitySidecars    CategoryName = "capability/sidecars"
+	CategoryCapabilityEvents      CategoryName = "capability/events"
+	CategoryCapabilityTelemetry   CategoryName = "capability/telemetry"
+	CategoryDatastore             CategoryName = "datastore"
+	CategorySubdomain             CategoryName = "subdomain"
+	CategoryDomain                CategoryName = "domain"
+	CategoryBlock                 CategoryName = "block"
 )
