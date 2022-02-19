151be43c1af931d5edca14a40e02a510f2b1b5c8
Brad Sickles <brad.sickles@gmail.com>
2022-02-19 09:18:04 -0500

    Replaced find.BlockModule with find.Module

--- find/module.go
+++ find/module.go
@@ -9,13 +9,13 @@ import (
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 )
 
-func BlockModule(cfg api.Config, block types.Block) (*types.Module, error) {
-	client := api.Client{Config: cfg}
-	ms, err := artifacts.ParseSource(block.ModuleSource)
+func Module(cfg api.Config, moduleSource string) (*types.Module, error) {
+	ms, err := artifacts.ParseSource(moduleSource)
 	if err != nil {
 		return nil, err
 	}
 
+	client := api.Client{Config: cfg}
 	module, err := client.Org(ms.OrgName).Modules().Get(ms.ModuleName)
 	var uae response.UnauthorizedError
 	if errors.As(err, &uae) {
@@ -23,9 +23,9 @@ func BlockModule(cfg api.Config, block types.Block) (*types.Module, error) {
 		module, err = client.Org(ms.OrgName).PublicModules().Get(ms.ModuleName)
 	}
 	if err != nil {
-		return nil, fmt.Errorf("error retrieving app %q module: %w", block.Name, err)
+		return nil, fmt.Errorf("error retrieving module: %w", err)
 	} else if module == nil {
-		return nil, fmt.Errorf("module %q does not exist in organization %q", block.ModuleSource, ms.OrgName)
+		return nil, fmt.Errorf("module %q does not exist in organization %q", moduleSource, ms.OrgName)
 	}
 	return module, nil
 }
