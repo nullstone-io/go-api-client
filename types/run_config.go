d322001a5057b22e8227dc407256b9d0d16b9957
Brad Sickles <brad.sickles@gmail.com>
2022-05-09 17:09:53 -0400

    Add DependencyConfigs to RunConfig

--- types/run_config.go
+++ types/run_config.go
@@ -5,14 +5,15 @@ import (
 )
 
 type RunConfig struct {
-	WorkspaceUid  uuid.UUID         `json:"workspaceUid"`
-	Source        string            `json:"source"`
-	SourceVersion string            `json:"sourceVersion"`
-	Variables     Variables         `json:"variables"`
-	EnvVariables  EnvVariables      `json:"envVariables"`
-	Connections   Connections       `json:"connections"`
-	Capabilities  CapabilityConfigs `json:"capabilities"`
-	Providers     Providers         `json:"providers"`
-	Targets       RunTargets        `json:"targets"`
-	Dependencies  Dependencies      `json:"dependencies"`
+	WorkspaceUid      uuid.UUID         `json:"workspaceUid"`
+	Source            string            `json:"source"`
+	SourceVersion     string            `json:"sourceVersion"`
+	Variables         Variables         `json:"variables"`
+	EnvVariables      EnvVariables      `json:"envVariables"`
+	Connections       Connections       `json:"connections"`
+	Capabilities      CapabilityConfigs `json:"capabilities"`
+	Providers         Providers         `json:"providers"`
+	Targets           RunTargets        `json:"targets"`
+	Dependencies      Dependencies      `json:"dependencies"`
+	DependencyConfigs DependencyConfigs `json:"dependencyConfigs" pg:"dependency_configs"`
 }
