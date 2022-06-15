5363305721347858c31276535a9fbd0f7f16beb2
Brad Sickles <brad.sickles@gmail.com>
2022-06-15 15:28:49 -0400

    Fix sorting of module versions

--- types/module_versions.go
+++ types/module_versions.go
@@ -3,12 +3,22 @@ package types
 import (
 	"golang.org/x/mod/semver"
 	"sort"
+	"strings"
 )
 
 type ModuleVersions []ModuleVersion
 
 var _ sort.Interface = ModuleVersions{}
 
-func (s ModuleVersions) Len() int           { return len(s) }
-func (s ModuleVersions) Less(i, j int) bool { return semver.Compare(s[i].Version, s[j].Version) < 0 }
-func (s ModuleVersions) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s ModuleVersions) Len() int { return len(s) }
+func (s ModuleVersions) Less(i, j int) bool {
+	return semver.Compare(validSemver(s[i].Version), validSemver(s[j].Version)) < 0
+}
+func (s ModuleVersions) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+func validSemver(version string) string {
+	if strings.HasPrefix(version, "v") {
+		return version
+	}
+	return "v" + version
+}
