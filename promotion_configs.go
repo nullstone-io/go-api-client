5177d556dabb83c13b8346fd7379232ee9639d94
Brad Sickles <brad.sickles@gmail.com>
2022-08-01 11:00:06 -0400

    Added support for module source override in promotion configs (#31)

--- promotion_configs.go
+++ promotion_configs.go
@@ -5,6 +5,7 @@ import (
 	"gopkg.in/nullstone-io/go-api-client.v0/response"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 	"net/http"
+	"net/url"
 )
 
 type PromotionConfigs struct {
@@ -15,8 +16,12 @@ func (s PromotionConfigs) path(stackId, blockId, envId int64) string {
 	return fmt.Sprintf("/orgs/%s/stacks/%d/blocks/%d/envs/%d/promotion-config", s.Client.Config.OrgName, stackId, blockId, envId)
 }
 
-func (s PromotionConfigs) Get(stackId, blockId, envId int64) (*types.RunConfig, error) {
-	res, err := s.Client.Do(http.MethodGet, s.path(stackId, blockId, envId), nil, nil, nil)
+func (s PromotionConfigs) Get(stackId, blockId, envId int64, moduleSourceOverride string) (*types.RunConfig, error) {
+	q := url.Values{}
+	if moduleSourceOverride != "" {
+		q.Set("module-source-override", moduleSourceOverride)
+	}
+	res, err := s.Client.Do(http.MethodGet, s.path(stackId, blockId, envId), q, nil, nil)
 	if err != nil {
 		return nil, err
 	}
