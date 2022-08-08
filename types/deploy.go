c3d04b8921ed24da2b74214f1ce44de5d12b97eb
Brad Sickles <brad.sickles@gmail.com>
2022-08-08 14:23:26 -0400

    Added new fields to deploy struct

--- types/deploy.go
+++ types/deploy.go
@@ -1,13 +1,24 @@
 package types
 
+import "time"
+
 type Deploy struct {
 	IdModel
-	OrgName   string `json:"orgName"`
-	StackId   int64  `json:"stackId"`
-	AppId     int64  `json:"appId"`
-	EnvId     int64  `json:"envId"`
-	Version   string `json:"version"`
-	Reference string `json:"reference"`
+	OrgName string `json:"orgName"`
+	StackId int64  `json:"stackId"`
+	AppId   int64  `json:"appId"`
+	EnvId   int64  `json:"envId"`
+
+	Reference     string    `json:"reference"`
+	Phase         string    `json:"phase"`
+	Status        string    `json:"status"`
+	StatusMessage string    `json:"statusMessage"`
+	StatusAt      time.Time `json:"statusAt"`
+
+	RepoUrl     string `json:"repoUrl"`
+	Version     string `json:"version"`
+	Type        string `json:"type"`
+	PackageMode string `json:"packageMode"`
 
 	App *Application `json:"app,omitempty"`
 	Env *Environment `json:"env,omitempty"`
