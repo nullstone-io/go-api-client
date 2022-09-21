89bf2bdaec43f2ce8fc14a196cc53869e89cb299
Brad Sickles <brad.sickles@gmail.com>
2022-09-21 11:07:31 -0400

    C1051 current outputs (#36)

--- response/read_json.go
+++ response/read_json.go
@@ -18,3 +18,37 @@ func ReadJson(res *http.Response, obj interface{}) error {
 	}
 	return nil
 }
+
+func ReadJsonVal[T any](res *http.Response) (T, error) {
+	var val T
+	if err := Verify(res); err != nil {
+		if IsNotFoundError(err) {
+			return val, nil
+		}
+		return val, err
+	}
+
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&val); err != nil {
+		return val, fmt.Errorf("error decoding json body: %w", err)
+	}
+	return val, nil
+}
+
+func ReadJsonPtr[T any](res *http.Response) (*T, error) {
+	if err := Verify(res); err != nil {
+		if IsNotFoundError(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+	var obj T
+	if err := decoder.Decode(&obj); err != nil {
+		return nil, fmt.Errorf("error decoding json body: %w", err)
+	}
+	return &obj, nil
+}
--- types/outputs.go
+++ types/outputs.go
@@ -1,10 +1,10 @@
 package types
 
-type Outputs map[string]OutputItem
+type Outputs map[string]Output
 
-type OutputItem struct {
-	Type      interface{} `json:"type"` // This is typically a string like "string", "number", but could also be ["tuple", "..."]
-	Value     interface{} `json:"value"`
-	Sensitive bool        `json:"sensitive"`
-	Redacted  bool        `json:"redacted"`
+type Output struct {
+	Type      any  `json:"type"` // This is typically a string like "string", "number", but could also be ["tuple", "..."]
+	Value     any  `json:"value"`
+	Sensitive bool `json:"sensitive"`
+	Redacted  bool `json:"redacted"`
 }
