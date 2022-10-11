02ee032c9fc32bcb0d25a1a8fe8d93b544f65ca4
Scott Sickles <scottsickles@gmail.com>
2022-10-11 14:47:20 -0400

    remove stackName from blocks

--- types/block.go
+++ types/block.go
@@ -5,7 +5,6 @@ type Block struct {
 	Type                string                      `json:"type"`
 	OrgName             string                      `json:"orgName"`
 	StackId             int64                       `json:"stackId"`
-	StackName           string                      `json:"stackName"`
 	Reference           string                      `json:"reference"`
 	Name                string                      `json:"name"`
 	ModuleSource        string                      `json:"moduleSource"`
