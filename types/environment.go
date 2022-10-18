dff0da9ab54c3c7db2c7763832d93e2e21f28d18
Scott Sickles <scottsickles@gmail.com>
2022-10-18 16:54:29 -0400

    add constants for each of the environment types

--- types/environment.go
+++ types/environment.go
@@ -1,12 +1,20 @@
 package types
 
+type EnvironmentType string
+
+const (
+	EnvTypePipeline EnvironmentType = "PipelineEnv"
+	EnvTypePreview  EnvironmentType = "PreviewEnv"
+	EnvTypeGlobal                   = "GlobalEnv"
+)
+
 type Environment struct {
 	IdModel
-	Type           string         `json:"type"`
-	Name           string         `json:"name"`
-	Reference      string         `json:"reference"`
-	OrgName        string         `json:"orgName"`
-	StackId        int64          `json:"stackId"`
-	ProviderConfig ProviderConfig `json:"providerConfig"`
-	PipelineOrder  *int           `json:"pipelineOrder"`
+	Type           string          `json:"type"`
+	Name           EnvironmentType `json:"name"`
+	Reference      string          `json:"reference"`
+	OrgName        string          `json:"orgName"`
+	StackId        int64           `json:"stackId"`
+	ProviderConfig ProviderConfig  `json:"providerConfig"`
+	PipelineOrder  *int            `json:"pipelineOrder"`
 }
