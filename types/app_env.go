a12ba665ad301b22fbee1c441f9d6305cf399c4b
Brad Sickles <brad.sickles@gmail.com>
2022-10-19 17:01:18 -0400

    Update AppEnv, Deploy types with build/deploy metadata

--- types/app_env.go
+++ types/app_env.go
@@ -2,9 +2,10 @@ package types
 
 type AppEnv struct {
 	IdModel
-	AppId   int64  `json:"appId"`
-	EnvId   int64  `json:"envId"`
-	Version string `json:"version"`
+	AppId     int64  `json:"appId"`
+	EnvId     int64  `json:"envId"`
+	Version   string `json:"version"`
+	CommitSha string `json:"commitSha"`
 
 	App *Application `json:"app,omitempty"`
 	Env *Environment `json:"env,omitempty"`
