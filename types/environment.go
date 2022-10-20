202b0a6d808a3419db154dbeb14c428420e3e10f
Scott Sickles <scottsickles@gmail.com>
2022-10-20 16:32:07 -0400

    C1111 preview envs (#41)
    squash merge C1111-preview-envs

--- types/environment.go
+++ types/environment.go
@@ -1,12 +1,20 @@
 package types
 
+type EnvironmentType string
+
+const (
+	EnvTypePipeline EnvironmentType = "PipelineEnv"
+	EnvTypePreview  EnvironmentType = "PreviewEnv"
+	EnvTypeGlobal   EnvironmentType = "GlobalEnv"
+)
+
 type Environment struct {
 	IdModel
-	Name           string         `json:"name"`
-	Reference      string         `json:"reference"`
-	OrgName        string         `json:"orgName"`
-	StackId        int64          `json:"stackId"`
-	StackName      string         `json:"stackName"`
-	ProviderConfig ProviderConfig `json:"providerConfig"`
-	PipelineOrder  int            `json:"pipelineOrder"`
+	Type           EnvironmentType `json:"type"`
+	Name           string          `json:"name"`
+	Reference      string          `json:"reference"`
+	OrgName        string          `json:"orgName"`
+	StackId        int64           `json:"stackId"`
+	ProviderConfig ProviderConfig  `json:"providerConfig"`
+	PipelineOrder  *int            `json:"pipelineOrder"`
 }
