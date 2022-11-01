1b8f7fbb511fc454448acd3e2d3808d370b51647
Brad Sickles <brad.sickles@gmail.com>
2022-11-01 15:37:49 -0400

    Added DestroyDependencies to run creation

--- types/create_run_input.go
+++ types/create_run_input.go
@@ -1,7 +1,16 @@
 package types
 
 type CreateRunInput struct {
-	IsDestroy         bool              `json:"isDestroy"`
+	// Create a run that destroys this workspace
+	IsDestroy bool `json:"isDestroy"`
+
+	// DestroyDependencies allows the user to identify which dependencies to destroy along with the block
+	// IsDestroy must be enabled for this field to have an effect
+	// `*` indicates attempt to destroy the workspace and its dependencies
+	// ``  indicates attempt to destroy only the specified workspace
+	// `<stack-id>/<block-id>/<env-id>,...` indicates a comma-delimited list of dependencies to destroy with the workspace
+	DestroyDependencies string `json:"destroyDependencies"`
+
 	IsApproved        *bool             `json:"isApproved"`
 	Source            string            `json:"source"`
 	SourceVersion     string            `json:"sourceVersion"`
