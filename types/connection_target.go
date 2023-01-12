2218e426aca59f1fb2c00c9ce1e9eb4c00a399e1
Scott Sickles <scottsickles@gmail.com>
2023-01-11 22:04:28 -0500

    dont send the blockId if it isnt populated

--- types/connection_target.go
+++ types/connection_target.go
@@ -2,7 +2,7 @@ package types
 
 type ConnectionTarget struct {
 	StackId   int64  `json:"stackId"`
-	BlockId   int64  `json:"blockId"`
+	BlockId   int64  `json:"blockId,omitempty"`
 	BlockName string `json:"blockName"`
 	EnvId     *int64 `json:"envId"`
 }
