9c9a6c2ee1b811363bda4d5d8bee3e2ec9a6caca
Scott Sickles <scottsickles@gmail.com>
2023-01-31 13:37:54 -0500

    copy-paste error fixed

--- workspace_module_version.go
+++ workspace_module_version.go
@@ -16,8 +16,8 @@ func (wmv WorkspaceModuleVersion) basePath(stackId, blockId, envId int64) string
 	return fmt.Sprintf("/orgs/%s/stacks/%d/blocks/%d/envs/%d/module-version", wmv.Client.Config.OrgName, stackId, blockId, envId)
 }
 
-func (wmv WorkspaceModuleVersion) Update(stackId, blockId, envId int64, input []types.VariableInput) (*types.WorkspaceChangeset, error) {
-	raw, _ := json.Marshal(input)
+func (wmv WorkspaceModuleVersion) Update(stackId, blockId, envId int64, version string) (*types.WorkspaceChangeset, error) {
+	raw, _ := json.Marshal(version)
 	res, err := wmv.Client.Do(http.MethodPut, wmv.basePath(stackId, blockId, envId), nil, nil, json.RawMessage(raw))
 	if err != nil {
 		return nil, err
