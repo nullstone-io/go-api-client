54f60fd0ae1cda4d33f71d8c6041de49572d4fff
Scott Sickles <scottsickles@gmail.com>
2023-03-31 09:47:54 -0400

    F1220 shared infra (#55)

--- types/block.go
+++ types/block.go
@@ -7,8 +7,20 @@ type Block struct {
 	StackId             int64                       `json:"stackId"`
 	Reference           string                      `json:"reference"`
 	Name                string                      `json:"name"`
+	IsShared            bool                        `json:"isShared"`
 	DnsName             string                      `json:"dnsName"`
 	ModuleSource        string                      `json:"moduleSource"`
 	ModuleSourceVersion string                      `json:"moduleSourceVersion"`
 	Connections         map[string]ConnectionTarget `json:"connections"`
+}
+
+type Blocks []Block
+
+func (b *Blocks) Find(stackId, blockId int64) *Block {
+	for _, block := range *b {
+		if block.StackId == stackId && block.Id == blockId {
+			return &block
+		}
+	}
+	return nil
 }
