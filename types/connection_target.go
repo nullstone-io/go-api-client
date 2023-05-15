e725f880127f4cee2d2345fdd28d2061fee41f32
Brad Sickles <brad.sickles@gmail.com>
2023-05-15 15:54:12 -0400

    Connection target help (#57)

--- types/connection_target.go
+++ types/connection_target.go
@@ -1,8 +1,78 @@
 package types
 
+import (
+	"strings"
+)
+
 type ConnectionTarget struct {
-	StackId   int64  `json:"stackId"`
+	StackId   int64  `json:"stackId,omitempty"`
+	StackName string `json:"stackName,omitempty"`
 	BlockId   int64  `json:"blockId,omitempty"`
-	BlockName string `json:"blockName"`
-	EnvId     *int64 `json:"envId"`
+	BlockName string `json:"blockName,omitempty"`
+	EnvId     *int64 `json:"envId,omitempty"`
+	EnvName   string `json:"envName,omitempty"`
+}
+
+func ParseConnectionTarget(s string) ConnectionTarget {
+	tokens := strings.Split(s, ".")
+	switch len(tokens) {
+	case 1:
+		return ConnectionTarget{
+			BlockName: tokens[0],
+		}
+	case 2:
+		return ConnectionTarget{
+			StackName: tokens[0],
+			BlockName: tokens[1],
+		}
+	case 3:
+		return ConnectionTarget{
+			StackName: tokens[0],
+			EnvName:   tokens[1],
+			BlockName: tokens[2],
+		}
+	default:
+		return ConnectionTarget{}
+	}
+}
+
+func (t ConnectionTarget) Normalize(stackId int64, blocks Blocks, sharedEnvId int64) ConnectionTarget {
+	result := t
+	if result.StackId == 0 {
+		result.StackId = stackId
+	}
+	if block := blocks.FindByName(result.BlockName); block != nil {
+		result.BlockId = block.Id
+		if block.IsShared && sharedEnvId != 0 {
+			result.EnvId = &sharedEnvId
+		}
+	}
+	return result
+}
+
+func (t ConnectionTarget) Match(other ConnectionTarget) bool {
+	if t.StackId != other.StackId {
+		return false
+	}
+	if t.BlockId != other.BlockId {
+		return false
+	}
+	if t.EnvId == nil {
+		return other.EnvId == nil
+	}
+	if other.EnvId == nil {
+		return false
+	}
+	return *t.EnvId == *other.EnvId
+}
+
+func (t ConnectionTarget) Workspace() WorkspaceTarget {
+	wt := WorkspaceTarget{
+		StackId: t.StackId,
+		BlockId: t.BlockId,
+	}
+	if t.EnvId != nil {
+		wt.EnvId = *t.EnvId
+	}
+	return wt
 }
