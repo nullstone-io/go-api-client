ace177a0d8ed44fa8687a42e74912901b9c4a307
Scott Sickles <scottsickles@gmail.com>
2023-06-14 12:04:54 -0400

    remove preview_envs#create endpoint; remove reference field on environment because it doesnt exist anymore; dont send pipelineOrder if nil

--- preview_envs.go
+++ preview_envs.go
@@ -8,11 +8,7 @@ import (
 	"net/http"
 )
 
-// when creating a preview environment, we must have a name
-type CreatePreviewEnvInput struct {
-	Name string `json:"name"`
-}
-
+// UpdatePreviewEnvInput
 // when updating, we don't have to pass a name
 // if you don't pass any fields, the api will simply make sure the preview environment is active
 type UpdatePreviewEnvInput struct {
@@ -23,23 +19,8 @@ type PreviewEnvs struct {
 	Client *Client
 }
 
-func (pe PreviewEnvs) basePath(stackId int64) string {
-	return fmt.Sprintf("orgs/%s/stacks/%d/preview_envs", pe.Client.Config.OrgName, stackId)
-}
-
 func (pe PreviewEnvs) envPath(stackId, envId int64) string {
 	return fmt.Sprintf("orgs/%s/stacks/%d/preview_envs/%d", pe.Client.Config.OrgName, stackId, envId)
-}
-
-// Create - POST /orgs/:orgName/stacks/:stack_id/preview_envs
-func (pe PreviewEnvs) Create(stackId int64, env *CreatePreviewEnvInput) (*types.Environment, error) {
-	rawPayload, _ := json.Marshal(env)
-	res, err := pe.Client.Do(http.MethodPost, pe.basePath(stackId), nil, nil, json.RawMessage(rawPayload))
-	if err != nil {
-		return nil, err
-	}
-
-	return response.ReadJsonPtr[types.Environment](res)
 }
 
 // Update - PUT/PATCH /orgs/:orgName/stacks/:stack_id/preview_envs/:id
--- types/environment.go
+++ types/environment.go
@@ -12,10 +12,9 @@ type Environment struct {
 	IdModel
 	Type           EnvironmentType `json:"type"`
 	Name           string          `json:"name"`
-	Reference      string          `json:"reference"`
 	OrgName        string          `json:"orgName"`
 	StackId        int64           `json:"stackId"`
 	ProviderConfig ProviderConfig  `json:"providerConfig"`
-	PipelineOrder  *int            `json:"pipelineOrder"`
+	PipelineOrder  *int            `json:"pipelineOrder,omitempty"`
 	ContextKey     string          `json:"contextKey"`
 }
