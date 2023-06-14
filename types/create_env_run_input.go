3fb2a7813d901e723010526509ecfd58b9b29916
Scott Sickles <scottsickles@gmail.com>
2023-06-14 17:04:49 -0400

    we need to pass the commit_sha for each app

--- types/create_env_run_input.go
+++ types/create_env_run_input.go
@@ -1,7 +1,11 @@
 package types
 
+type AppRunInput struct {
+	Id        int64  `json:"id"`
+	CommitSha string `json:"commitSha"`
+}
+
 type CreateEnvRunInput struct {
-	IsDestroy bool     `json:"isDestroy"`
-	AppIds    *[]int64 `json:"appIds"`
-	CommitSha *string  `json:"commitSha"`
+	IsDestroy bool           `json:"isDestroy"`
+	Apps      *[]AppRunInput `json:"apps"`
 }
