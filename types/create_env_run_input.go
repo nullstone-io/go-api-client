be73a7b5de254c62ab5ee14e6325f749f1ff306f
Scott Sickles <scottsickles@gmail.com>
2023-06-14 17:40:14 -0400

    just kidding, we dont need the commit_shas for each app

--- types/create_env_run_input.go
+++ types/create_env_run_input.go
@@ -1,11 +1,6 @@
 package types
 
-type AppRunInput struct {
-	Id        int64  `json:"id"`
-	CommitSha string `json:"commitSha"`
-}
-
 type CreateEnvRunInput struct {
-	IsDestroy bool           `json:"isDestroy"`
-	Apps      *[]AppRunInput `json:"apps"`
+	IsDestroy bool     `json:"isDestroy"`
+	AppIds    *[]int64 `json:"appIds"`
 }
