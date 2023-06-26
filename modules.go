e9c22be5cdf273aa2a06be4d90b31f19e68d1228
Brad Sickles <brad.sickles@gmail.com>
2023-06-26 11:11:12 -0400

    Require orgName in module/module_versions endpoints

--- modules.go
+++ modules.go
@@ -12,33 +12,33 @@ type Modules struct {
 	Client *Client
 }
 
-func (m Modules) basePath() string {
-	return fmt.Sprintf("orgs/%s/modules", m.Client.Config.OrgName)
+func (m Modules) basePath(orgName string) string {
+	return fmt.Sprintf("orgs/%s/modules", orgName)
 }
 
-func (m Modules) path(moduleName string) string {
-	return fmt.Sprintf("orgs/%s/modules/%s", m.Client.Config.OrgName, moduleName)
+func (m Modules) path(orgName, moduleName string) string {
+	return fmt.Sprintf("orgs/%s/modules/%s", orgName, moduleName)
 }
 
-func (m Modules) List() ([]types.Module, error) {
-	res, err := m.Client.Do(http.MethodGet, m.basePath(), nil, nil, nil)
+func (m Modules) List(orgName string) ([]types.Module, error) {
+	res, err := m.Client.Do(http.MethodGet, m.basePath(orgName), nil, nil, nil)
 	if err != nil {
 		return nil, err
 	}
 	return response.ReadJsonVal[[]types.Module](res)
 }
 
-func (m Modules) Get(moduleName string) (*types.Module, error) {
-	res, err := m.Client.Do(http.MethodGet, m.path(moduleName), nil, nil, nil)
+func (m Modules) Get(orgName, moduleName string) (*types.Module, error) {
+	res, err := m.Client.Do(http.MethodGet, m.path(orgName, moduleName), nil, nil, nil)
 	if err != nil {
 		return nil, err
 	}
 	return response.ReadJsonPtr[types.Module](res)
 }
 
-func (m Modules) Create(module *types.Module) error {
+func (m Modules) Create(orgName string, module *types.Module) error {
 	rawPayload, _ := json.Marshal(module)
-	res, err := m.Client.Do(http.MethodPost, m.basePath(), nil, nil, json.RawMessage(rawPayload))
+	res, err := m.Client.Do(http.MethodPost, m.basePath(orgName), nil, nil, json.RawMessage(rawPayload))
 	if err != nil {
 		return err
 	}
