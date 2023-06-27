fed4212a7b1d188ebf3e73900f10672a0b09b495
Brad Sickles <brad.sickles@gmail.com>
2023-06-27 13:04:03 -0400

    Added helper to retrieve jwt.StandardClaims from access token

---
+++ auth/get_claims.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/cristalhq/jwt/v3"
+)
+
+var (
+	NotValidJwtTokenErr = errors.New("access token is not a valid JWT Token")
+)
+
+// GetClaims attempts to read the access token and parse jwt.StandardClaims
+// If the access token is not a valid JWT, this will return NotValidJwtTokenErr
+func GetClaims(source AccessTokenSource, orgName string) (*jwt.StandardClaims, error) {
+	accessToken, err := source.GetAccessToken(orgName)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := jwt.ParseString(accessToken)
+	if err != nil {
+		return nil, NotValidJwtTokenErr
+	}
+
+	var claims jwt.StandardClaims
+	if err := json.Unmarshal(token.RawClaims(), &claims); err != nil {
+		return nil, NotValidJwtTokenErr
+	}
+	return &claims, nil
+}
