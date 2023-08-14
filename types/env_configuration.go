3b677f863705e99e7b1892a185384f2bff988ef1
Scott Sickles <scottsickles@gmail.com>
2023-08-14 00:14:01 -0400

    serialize env_configurations to json

--- types/env_configuration.go
+++ types/env_configuration.go
@@ -1,37 +1,37 @@
 package types
 
 type EnvConfiguration struct {
-	Version      string                            `yaml:"version"`
-	Subdomains   map[string]SubdomainConfiguration `yaml:"subdomains"`
-	Applications map[string]AppConfiguration       `yaml:"apps"`
+	Version      string                            `yaml:"version" json:"version"`
+	Subdomains   map[string]SubdomainConfiguration `yaml:"subdomains" json:"subdomains"`
+	Applications map[string]AppConfiguration       `yaml:"apps" json:"apps"`
 }
 
 type SubdomainConfiguration struct {
-	Name                string            `yaml:"-"`
-	DnsName             string            `yaml:"dns_name"`
-	ModuleSource        string            `yaml:"module"`
-	ModuleSourceVersion *string           `yaml:"module_version"`
-	Variables           map[string]any    `yaml:"vars"`
-	Connections         ConnectionTargets `yaml:"connections"`
+	Name                string            `yaml:"-" json:"name"`
+	DnsName             string            `yaml:"dns_name" json:"dnsName"`
+	ModuleSource        string            `yaml:"module" json:"module"`
+	ModuleSourceVersion *string           `yaml:"module_version" json:"moduleVersion"`
+	Variables           map[string]any    `yaml:"vars" json:"vars"`
+	Connections         ConnectionTargets `yaml:"connections" json:"connections"`
 }
 
 type AppConfiguration struct {
-	Name                string                   `yaml:"-"`
-	ModuleSource        string                   `yaml:"module"`
-	ModuleSourceVersion *string                  `yaml:"module_version"`
-	Variables           map[string]any           `yaml:"vars"`
-	Capabilities        CapabilityConfigurations `yaml:"capabilities"`
-	EnvVariables        map[string]string        `yaml:"environment"`
+	Name                string                   `yaml:"-" json:"name"`
+	ModuleSource        string                   `yaml:"module" json:"module"`
+	ModuleSourceVersion *string                  `yaml:"module_version" json:"moduleVersion"`
+	Variables           map[string]any           `yaml:"vars" json:"vars"`
+	Capabilities        CapabilityConfigurations `yaml:"capabilities" json:"capabilities"`
+	EnvVariables        map[string]string        `yaml:"environment" json:"envVars"`
 }
 
 type CapabilityConfigurations []CapabilityConfiguration
 
 type CapabilityConfiguration struct {
-	ModuleSource        string            `yaml:"module"`
-	ModuleSourceVersion *string           `yaml:"module_version"`
-	Variables           map[string]any    `yaml:"vars"`
-	Connections         ConnectionTargets `yaml:"connections"`
-	Namespace           *string           `yaml:"namespace"`
+	ModuleSource        string            `yaml:"module" json:"module"`
+	ModuleSourceVersion *string           `yaml:"module_version" json:"moduleVersion"`
+	Variables           map[string]any    `yaml:"vars" json:"vars"`
+	Connections         ConnectionTargets `yaml:"connections" json:"connections"`
+	Namespace           *string           `yaml:"namespace" json:"namespace"`
 }
 
 type ConnectionTargets map[string]ConnectionTarget
