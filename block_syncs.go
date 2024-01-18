71650fbba121be98a34636b1091d535ddec3afa6
Scott Sickles <scottsickles@gmail.com>
2024-01-18 15:37:10 -0500

    flip flopped the use of env_id between block_syncs and pipeline_block_syncs

--- block_syncs.go
+++ block_syncs.go
@@ -16,14 +16,14 @@ type BlockSyncPayload struct {
 	Blocks []types.Block `json:"blocks"`
 }
 
-func (s BlockSyncs) basePath(stackId int64, repo string) string {
-	return fmt.Sprintf("orgs/%s/stacks/%d/repos/%s/block_syncs", s.Client.Config.OrgName, stackId, repo)
+func (s BlockSyncs) basePath(stackId, envId int64, repo string) string {
+	return fmt.Sprintf("orgs/%s/stacks/%d/envs/%d/repos/%s/block_syncs", s.Client.Config.OrgName, stackId, envId, repo)
 }
 
 // Create - POST /orgs/:orgName/stacks/:stack_id/repos/:repo_name/block_syncs
-func (s BlockSyncs) Create(stackId int64, repo string, payload BlockSyncPayload) ([]types.Block, error) {
+func (s BlockSyncs) Create(stackId, envId int64, repo string, payload BlockSyncPayload) ([]types.Block, error) {
 	rawPayload, _ := json.Marshal(payload)
-	res, err := s.Client.Do(http.MethodPost, s.basePath(stackId, repo), nil, nil, json.RawMessage(rawPayload))
+	res, err := s.Client.Do(http.MethodPost, s.basePath(stackId, envId, repo), nil, nil, json.RawMessage(rawPayload))
 	if err != nil {
 		return nil, err
 	}
--- pipeline_block_syncs.go
+++ pipeline_block_syncs.go
@@ -12,14 +12,14 @@ type PipelineBlockSyncs struct {
 	Client *Client
 }
 
-func (p PipelineBlockSyncs) basePath(stackId, envId int64, repo string) string {
-	return fmt.Sprintf("orgs/%s/stacks/%d/envs/%d/repos/%s/block_syncs", p.Client.Config.OrgName, stackId, envId, repo)
+func (p PipelineBlockSyncs) basePath(stackId int64, repo string) string {
+	return fmt.Sprintf("orgs/%s/stacks/%d/repos/%s/block_syncs", p.Client.Config.OrgName, stackId, repo)
 }
 
 // Create - POST /orgs/:orgName/stacks/:stack_id/envs/:env_id/repos/:repo_name/block_syncs
-func (p PipelineBlockSyncs) Create(stackId, envId int64, repo string, payload BlockSyncPayload) ([]types.Block, error) {
+func (p PipelineBlockSyncs) Create(stackId int64, repo string, payload BlockSyncPayload) ([]types.Block, error) {
 	rawPayload, _ := json.Marshal(payload)
-	res, err := p.Client.Do(http.MethodPost, p.basePath(stackId, envId, repo), nil, nil, json.RawMessage(rawPayload))
+	res, err := p.Client.Do(http.MethodPost, p.basePath(stackId, repo), nil, nil, json.RawMessage(rawPayload))
 	if err != nil {
 		return nil, err
 	}
