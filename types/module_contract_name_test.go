245c13843e2f8082806f52c6273957cf80b0e90c
Brad Sickles <brad.sickles@gmail.com>
2024-01-19 12:45:02 -0500

    Created method to sort module contract names

--- types/module_contract_name_test.go
+++ types/module_contract_name_test.go
@@ -1,7 +1,9 @@
 package types
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"sort"
 	"testing"
 )
 
@@ -33,4 +35,92 @@ func TestModuleContractName_Match(t *testing.T) {
 			assert.True(t, result)
 		})
 	}
+}
+
+func TestCompareModuleContractName(t *testing.T) {
+	tests := []struct {
+		a        string
+		b        string
+		wantLess bool
+	}{
+		{
+			a:        "app:server/aws/ec2:beanstalk",
+			b:        "app:server/aws/ec2:*",
+			wantLess: true,
+		},
+		{
+			a:        "app:container/aws/ecs:fargate",
+			b:        "app:container/aws/ecs:*",
+			wantLess: true,
+		},
+		{
+			a:        "app:container/aws/ecs:fargate",
+			b:        "app:container/aws/ecs",
+			wantLess: true,
+		},
+		{
+			a:        "app:container/aws/ecs:fargate",
+			b:        "app:container/aws/ecs:*",
+			wantLess: true,
+		},
+		{
+			a:        "app:container/aws/ecs:fargate",
+			b:        "app:container/aws/ecs:ec2",
+			wantLess: false,
+		},
+		{
+			a:        "app:container/aws/ec2",
+			b:        "app:server/aws/ecs",
+			wantLess: true,
+		},
+		{
+			a:        "app:container/aws/k8s",
+			b:        "app:container/gcp/k8s",
+			wantLess: true,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			a, err := ParseModuleContractName(test.a)
+			if err != nil {
+				t.Fatalf("bad contract name (a): %s", err)
+			}
+			b, err := ParseModuleContractName(test.b)
+			if err != nil {
+				t.Fatalf("bad contract name (b): %s", err)
+			}
+			got := CompareModuleContractName(a, b)
+			assert.Equal(t, test.wantLess, got)
+		})
+	}
+}
+
+func TestCompareModuleContractName_Sort(t *testing.T) {
+	all := []string{
+		"app:serverless/aws/lambda:*",
+		"app:server/aws/ec2:*",
+		"app:container/aws/ecs:*",
+		"app:container/gcp/k8s:gke",
+		"app:static-site/aws/s3:*",
+		"app:server/aws/ec2:beanstalk",
+	}
+	want := []string{
+		"app:container/aws/ecs:*",
+		"app:container/gcp/k8s:gke",
+		"app:server/aws/ec2:beanstalk",
+		"app:server/aws/ec2:*",
+		"app:serverless/aws/lambda:*",
+		"app:static-site/aws/s3:*",
+	}
+	got := make([]string, len(all))
+	copy(got, all)
+
+	sort.SliceStable(got, func(i, j int) bool {
+		a, _ := ParseModuleContractName(got[i])
+		b, _ := ParseModuleContractName(got[j])
+		return CompareModuleContractName(a, b)
+	})
+
+	assert.Equal(t, want, got)
 }
