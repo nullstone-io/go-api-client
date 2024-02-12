a965121936f69c643d5251e74e63350460820ba1
Scott Sickles <scottsickles@gmail.com>
2024-02-12 16:28:20 -0500

    add a new input for commitSha so we can make sure to capture this anytime we able to

--- deploys.go
+++ deploys.go
@@ -12,6 +12,19 @@ type Deploys struct {
 	Client *Client
 }
 
+// DeployCreatePayload is the payload for creating a deploy
+//
+//		If `FromSource` is specified, we deploy from the latest commit on the configured branch for this environment
+//		Otherwise, version is required
+//		commitSha and reference are optional and get populated on the deploy no matter whether we are deploying
+//	   fromSource or by version
+type DeployCreatePayload struct {
+	FromSource bool   `json:"fromSource"`
+	CommitSha  string `json:"commitSha"`
+	Version    string `json:"version"`
+	Reference  string `json:"reference"`
+}
+
 func (d Deploys) basePath(stackId, appId, envId int64) string {
 	return fmt.Sprintf("orgs/%s/stacks/%d/apps/%d/envs/%d/deploys", d.Client.Config.OrgName, stackId, appId, envId)
 }
@@ -20,11 +33,8 @@ func (d Deploys) path(stackId, appId, envId, deployId int64) string {
 	return fmt.Sprintf("orgs/%s/stacks/%d/apps/%d/envs/%d/deploys/%d", d.Client.Config.OrgName, stackId, appId, envId, deployId)
 }
 
-func (d Deploys) Create(stackId, appId, envId int64, version string, fromSource bool) (*types.Deploy, error) {
-	rawPayload, _ := json.Marshal(map[string]interface{}{
-		"version":    version,
-		"fromSource": fromSource,
-	})
+func (d Deploys) Create(stackId, appId, envId int64, payload DeployCreatePayload) (*types.Deploy, error) {
+	rawPayload, _ := json.Marshal(payload)
 	res, err := d.Client.Do(http.MethodPost, d.basePath(stackId, appId, envId), nil, nil, json.RawMessage(rawPayload))
 	if err != nil {
 		return nil, err
