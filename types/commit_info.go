95db3e00441d8f7097649700215deaa7794f385f
Scott Sickles <scottsickles@gmail.com>
2024-04-26 17:16:51 -0400

    add branch name

--- types/commit_info.go
+++ types/commit_info.go
@@ -4,6 +4,7 @@ type CommitInfo struct {
 	RepoOwner   string `json:"repoOwner"`
 	RepoName    string `json:"repoName"`
 	RepoUrl     string `json:"repoUrl"`
+	BranchName  string `json:"branchName"`
 	CommitSha   string `json:"commitSha"`
 	CommitUrl   string `json:"commitUrl"`
 	VcsUsername string `json:"vcsUsername"`
