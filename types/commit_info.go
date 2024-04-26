f17d762eb24cceaf223dc342429513f49dccc2df
Scott Sickles <scottsickles@gmail.com>
2024-04-26 17:33:09 -0400

    updated with additional fields for provider and repo

--- types/commit_info.go
+++ types/commit_info.go
@@ -1,8 +1,14 @@
 package types
 
+const (
+	CommitInfoVcsProviderGithub = "github"
+)
+
 type CommitInfo struct {
+	VcsProvider string `json:"vcsProvider"`
 	RepoOwner   string `json:"repoOwner"`
 	RepoName    string `json:"repoName"`
+	Repo        string `json:"repo"`
 	RepoUrl     string `json:"repoUrl"`
 	BranchName  string `json:"branchName"`
 	CommitSha   string `json:"commitSha"`
