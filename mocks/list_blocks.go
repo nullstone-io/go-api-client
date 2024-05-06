6881450e26b99b8e3f213af3752911840073323c
Brad Sickles <brad.sickles@gmail.com>
2024-05-06 11:25:52 -0400

    Mocks (#80)

--- mocks/list_blocks.go
+++ mocks/list_blocks.go
@@ -34,4 +34,61 @@ func ListBlocks(router *mux.Router, blocks []types.Block) {
 			raw, _ := json.Marshal(result)
 			w.Write(raw)
 		})
+	router.Methods(http.MethodGet).
+		Path("/orgs/{orgName}/stacks/{stackId}/blocks/{blockId}").
+		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			vars := mux.Vars(r)
+			orgName, rawStackId, rawBlockId := vars["orgName"], vars["stackId"], vars["blockId"]
+			stackId, _ := strconv.ParseInt(rawStackId, 10, 64)
+			blockId, _ := strconv.ParseInt(rawBlockId, 10, 64)
+
+			for _, block := range blocks {
+				if block.OrgName == orgName && block.StackId == stackId && block.Id == blockId {
+					raw, _ := json.Marshal(block)
+					w.Write(raw)
+					return
+				}
+			}
+		})
+
+	router.Methods(http.MethodGet).
+		Path("/orgs/{orgName}/stacks/{stackId}/apps").
+		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			vars := mux.Vars(r)
+			orgName := vars["orgName"]
+			var stackId int64
+			if val, err := strconv.ParseInt(vars["stackId"], 10, 64); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(fmt.Sprintf("Invalid stackId: %s", err)))
+				return
+			} else {
+				stackId = val
+			}
+
+			result := make([]types.Block, 0)
+			for _, block := range blocks {
+				if block.Type == "Application" && block.OrgName == orgName && block.StackId == stackId {
+					result = append(result, block)
+				}
+			}
+
+			raw, _ := json.Marshal(result)
+			w.Write(raw)
+		})
+	router.Methods(http.MethodGet).
+		Path("/orgs/{orgName}/stacks/{stackId}/apps/{appId}").
+		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			vars := mux.Vars(r)
+			orgName, rawStackId, rawBlockId := vars["orgName"], vars["stackId"], vars["appId"]
+			stackId, _ := strconv.ParseInt(rawStackId, 10, 64)
+			blockId, _ := strconv.ParseInt(rawBlockId, 10, 64)
+
+			for _, block := range blocks {
+				if block.Type == "Application" && block.OrgName == orgName && block.StackId == stackId && block.Id == blockId {
+					raw, _ := json.Marshal(block)
+					w.Write(raw)
+					return
+				}
+			}
+		})
 }
--- mocks/list_workspaces.go
+++ mocks/list_workspaces.go
@@ -9,9 +9,9 @@ import (
 	"strconv"
 )
 
-func ListEnvironments(router *mux.Router, envs []types.Environment) {
+func ListWorkspaces(router *mux.Router, workspaces []types.Workspace) {
 	router.Methods(http.MethodGet).
-		Path("/orgs/{orgName}/stacks/{stackId}/envs").
+		Path("/orgs/{orgName}/stacks/{stackId}/workspaces").
 		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			vars := mux.Vars(r)
 			orgName := vars["orgName"]
@@ -24,10 +24,10 @@ func ListEnvironments(router *mux.Router, envs []types.Environment) {
 				stackId = val
 			}
 
-			result := make([]types.Environment, 0)
-			for _, env := range envs {
-				if env.OrgName == orgName && env.StackId == stackId {
-					result = append(result, env)
+			result := make([]types.Workspace, 0)
+			for _, ws := range workspaces {
+				if ws.OrgName == orgName && ws.StackId == stackId {
+					result = append(result, ws)
 				}
 			}
 
@@ -35,16 +35,17 @@ func ListEnvironments(router *mux.Router, envs []types.Environment) {
 			w.Write(raw)
 		})
 	router.Methods(http.MethodGet).
-		Path("/orgs/{orgName}/stacks/{stackId}/envs/{envId}").
+		Path("/orgs/{orgName}/stacks/{stackId}/blocks/{blockId}/envs/{envId}").
 		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			vars := mux.Vars(r)
-			orgName, rawStackId, rawEnvId := vars["orgName"], vars["stackId"], vars["envId"]
+			orgName, rawStackId, rawBlockId, rawEnvId := vars["orgName"], vars["stackId"], vars["blockId"], vars["envId"]
 			stackId, _ := strconv.ParseInt(rawStackId, 10, 64)
+			blockId, _ := strconv.ParseInt(rawBlockId, 10, 64)
 			envId, _ := strconv.ParseInt(rawEnvId, 10, 64)
 
-			for _, env := range envs {
-				if env.OrgName == orgName && env.StackId == stackId && env.Id == envId {
-					raw, _ := json.Marshal(env)
+			for _, ws := range workspaces {
+				if ws.OrgName == orgName && ws.StackId == stackId && ws.BlockId == blockId && ws.EnvId == envId {
+					raw, _ := json.Marshal(ws)
 					w.Write(raw)
 					return
 				}
