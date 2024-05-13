efd94032bfb01c50586d410f4bfece4bac8f776b
Scott Sickles <scottsickles@gmail.com>
2024-05-12 23:04:34 -0400

    added paging to the workspace_workflows endpoint

--- workflows.go
+++ workflows.go
@@ -6,18 +6,26 @@ import (
 	"gopkg.in/nullstone-io/go-api-client.v0/response"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 	"net/http"
+	"net/url"
 )
 
 type WorkspaceWorkflows struct {
 	Client *Client
 }
 
-func (w WorkspaceWorkflows) basePath(orgName string, stackId, blockId, envId int64) string {
-	return fmt.Sprintf("orgs/%s/stacks/%d/blocks/%d/envs/%d/workspace_workflows", orgName, stackId, blockId, envId)
+func (w WorkspaceWorkflows) basePath(stackId, blockId, envId int64) string {
+	return fmt.Sprintf("orgs/%s/stacks/%d/blocks/%d/envs/%d/workspace_workflows", w.Client.Config.OrgName, stackId, blockId, envId)
 }
 
-func (w WorkspaceWorkflows) List(ctx context.Context, orgName string, stackId, blockId, envId int64) ([]types.WorkspaceWorkflowStatus, error) {
-	res, err := w.Client.Do(ctx, http.MethodGet, w.basePath(orgName, stackId, blockId, envId), nil, nil, nil)
+func (w WorkspaceWorkflows) List(ctx context.Context, stackId, blockId, envId int64, page, pageSize *int64) ([]types.WorkspaceWorkflowStatus, error) {
+	query := url.Values{}
+	if page != nil {
+		query.Set("page", fmt.Sprint(*page))
+	}
+	if pageSize != nil {
+		query.Set("pageSize", fmt.Sprint(*pageSize))
+	}
+	res, err := w.Client.Do(ctx, http.MethodGet, w.basePath(stackId, blockId, envId), query, nil, nil)
 	if err != nil {
 		return nil, err
 	}
