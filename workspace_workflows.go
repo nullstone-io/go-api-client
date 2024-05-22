629758b7e15c3a46fa0fdb01c61c3077449e864d
Brad Sickles <brad.sickles@gmail.com>
2024-05-22 12:11:03 -0400

    Drop Create, add List

--- workspace_workflows.go
+++ workspace_workflows.go
@@ -2,11 +2,12 @@ package api
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"gopkg.in/nullstone-io/go-api-client.v0/response"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 type WorkspaceWorkflows struct {
@@ -17,11 +18,17 @@ func (ww WorkspaceWorkflows) basePath(stackId, blockId, envId int64) string {
 	return fmt.Sprintf("/orgs/%s/stacks/%d/blocks/%d/envs/%d/workspace_workflows", ww.Client.Config.OrgName, stackId, blockId, envId)
 }
 
-func (ww WorkspaceWorkflows) Create(ctx context.Context, stackId, blockId, envId int64, input types.CreateWorkspaceWorkflowPayload) (*types.WorkspaceWorkflow, error) {
-	raw, _ := json.Marshal(input)
-	res, err := ww.Client.Do(ctx, http.MethodPost, ww.basePath(stackId, blockId, envId), nil, nil, json.RawMessage(raw))
+func (ww WorkspaceWorkflows) List(ctx context.Context, stackId, blockId, envId int64, page, perPage int) ([]types.WorkspaceWorkflow, error) {
+	q := url.Values{}
+	if page > 0 {
+		q.Set("page", strconv.Itoa(page))
+	}
+	if perPage > 0 {
+		q.Set("perPage", strconv.Itoa(perPage))
+	}
+	res, err := ww.Client.Do(ctx, http.MethodGet, ww.basePath(stackId, blockId, envId), q, nil, nil)
 	if err != nil {
 		return nil, err
 	}
-	return response.ReadJsonPtr[types.WorkspaceWorkflow](res)
+	return response.ReadJsonVal[[]types.WorkspaceWorkflow](res)
 }
