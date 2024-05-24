ca2139585a5f588716d06ad5c4c28fd14683172d
Brad Sickles <brad.sickles@gmail.com>
2024-05-24 16:44:33 -0400

    Added CreatedBy to IdModel

--- types/id_model.go
+++ types/id_model.go
@@ -5,5 +5,6 @@ import "time"
 type IdModel struct {
 	Id        int64     `json:"id,omitempty"`
 	CreatedAt time.Time `json:"createdAt"`
+	CreatedBy string    `json:"createdBy"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
