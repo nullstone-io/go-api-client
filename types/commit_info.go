8f809081b4bcaf206dbb8844004550cbb1a21578
Brad Sickles <brad.sickles@gmail.com>
2024-06-06 17:21:19 -0400

    Added PRTitle and CommitMessage to CommitInfo

--- types/commit_info.go
+++ types/commit_info.go
@@ -11,16 +11,18 @@ const (
 )
 
 type CommitInfo struct {
-	Type        string `json:"type"`
-	VcsProvider string `json:"vcsProvider"`
-	RepoOwner   string `json:"repoOwner"`
-	RepoName    string `json:"repoName"`
-	Repo        string `json:"repo"`
-	RepoUrl     string `json:"repoUrl"`
-	BranchName  string `json:"branchName"`
-	CommitSha   string `json:"commitSha"`
-	CommitUrl   string `json:"commitUrl"`
-	VcsUsername string `json:"vcsUsername"`
-	PRNumber    int    `json:"prNumber"`
-	PRId        int64  `json:"prId"`
+	Type          string `json:"type"`
+	VcsProvider   string `json:"vcsProvider"`
+	RepoOwner     string `json:"repoOwner"`
+	RepoName      string `json:"repoName"`
+	Repo          string `json:"repo"`
+	RepoUrl       string `json:"repoUrl"`
+	BranchName    string `json:"branchName"`
+	CommitSha     string `json:"commitSha"`
+	CommitUrl     string `json:"commitUrl"`
+	CommitMessage string `json:"commitMessage"`
+	VcsUsername   string `json:"vcsUsername"`
+	PRNumber      int    `json:"prNumber"`
+	PRId          int64  `json:"prId"`
+	PRTitle       string `json:"prTitle"`
 }
