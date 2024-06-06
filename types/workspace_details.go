ff13c52614db0f38e95bbe99252c243f13f6a6dd
Brad Sickles <brad.sickles@gmail.com>
2024-06-06 17:08:03 -0400

    Added WorkspaceDetails endpoint (#87)

---
+++ types/workspace_details.go
@@ -0,0 +1,49 @@
+package types
+
+import (
+	"encoding/json"
+)
+
+type WorkspaceDetails struct {
+	OrgName     string      `json:"orgName"`
+	Stack       Stack       `json:"stack"`
+	Environment Environment `json:"environment"`
+	// BlockRaw is a json.RawMessage because it is a polymorphic type
+	// Use Block() to get a concrete type, but returned as "any" value
+	// Use BlockType() to get the string type
+	BlockRaw json.RawMessage `json:"block"`
+}
+
+func (d WorkspaceDetails) BlockType() BlockType {
+	var tmp struct {
+		Type BlockType `json:"type"`
+	}
+	json.Unmarshal(d.BlockRaw, &tmp)
+	if tmp.Type == "" {
+		tmp.Type = BlockTypeBlock
+	}
+	return tmp.Type
+}
+
+// Block parses the block into the concrete type (e.g. Block, Application, etc.)
+// You can use `block, ok := result.(Block)` or `switch result.(type)`
+func (d WorkspaceDetails) Block() any {
+	switch d.BlockType() {
+	case BlockTypeApplication:
+		var val Application
+		json.Unmarshal(d.BlockRaw, &val)
+		return val
+	case BlockTypeSubdomain:
+		var val Subdomain
+		json.Unmarshal(d.BlockRaw, &val)
+		return val
+	case BlockTypeDomain:
+		var val Domain
+		json.Unmarshal(d.BlockRaw, &val)
+		return val
+	default:
+		var val Block
+		json.Unmarshal(d.BlockRaw, &val)
+		return val
+	}
+}
