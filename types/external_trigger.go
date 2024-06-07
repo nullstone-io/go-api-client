badf6bb9a3e078d2c36b5c6463ccda8e9c6a8877
Brad Sickles <brad.sickles@gmail.com>
2024-06-07 13:15:39 -0400

    Added ExternalTrigger type

---
+++ types/external_trigger.go
@@ -0,0 +1,35 @@
+package types
+
+const (
+	ExternalTriggerSourceManual = "manual"
+	ExternalTriggerSourceGithub = "github"
+)
+
+const (
+	ExternalTriggerEventUser               = "user"
+	ExternalTriggerEventVcsPush            = "vcs-push"
+	ExternalTriggerEventVcsOpenPullRequest = "vcs-open-pull-request"
+)
+
+// ExternalTrigger represents the source of an external trigger performing an action against Nullstone
+type ExternalTrigger struct {
+	// Source is the origination of the ExternalTrigger
+	// Examples: ExternalTriggerSourceManual, ExternalTriggerSourceGithub
+	Source string `json:"source"`
+	// Event indicates what type of event caused the ExternalTrigger
+	// Examples: ExternalTriggerEventUser, ExternalTriggerEventVcsPush, ExternalTriggerEventVcsOpenPullRequest
+	Event string `json:"event"`
+	// SourceUser indicates the user that caused the ExternalTrigger
+	// This user is dependent on the Source
+	// For example, if ExternalTriggerSourceGithub, this is the Github user
+	SourceUser ExternalTriggerUser `json:"sourceUser"`
+}
+
+type ExternalTriggerUser struct {
+	// Id is the platform-dependent identification
+	// Many platforms use int64, but string is universal to all platforms
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	AvatarUrl string `json:"avatarUrl"`
+}
