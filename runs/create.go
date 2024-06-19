8b98d33fe2d4db0fc22b613538264bd7e3a68258
Brad Sickles <brad.sickles@gmail.com>
2024-06-19 19:58:41 -0400

    correct name

--- runs/create.go
+++ runs/create.go
@@ -16,9 +16,9 @@ func Create(ctx context.Context, cfg api.Config, workspace types.Workspace, comm
 	}
 
 	client := api.Client{Config: cfg}
-	newRun, err := client.Runs().Create(ctx, workspace.StackId, workspace.Uid, input)
+	result, err := client.Runs().Create(ctx, workspace.StackId, workspace.Uid, input)
 	if err != nil {
 		return nil, fmt.Errorf("error creating run: %w", err)
 	}
-	return newRun, nil
+	return result, nil
 }
