3ae24879aa8f0c0602c49454c1db2e220f79c139
Brad Sickles <brad.sickles@gmail.com>
2024-06-19 08:41:22 -0400

    Added IntentWorkflows.List, IntentWorkflows.Get

--- client.go
+++ client.go
@@ -38,167 +38,126 @@ func (c *Client) CurrentUser() CurrentUser {
 func (c *Client) Organizations() Organizations {
 	return Organizations{Client: c}
 }
-
 func (c *Client) Stacks() Stacks {
 	return Stacks{Client: c}
 }
-
 func (c *Client) StacksByName() StacksByName {
 	return StacksByName{Client: c}
 }
-
 func (c *Client) Environments() Environments {
 	return Environments{Client: c}
 }
-
 func (c *Client) PreviewEnvs() PreviewEnvs {
 	return PreviewEnvs{Client: c}
 }
-
 func (c *Client) EnvironmentsByName() EnvironmentsByName {
 	return EnvironmentsByName{Client: c}
 }
-
 func (c *Client) EnvInfraConfigurations() EnvInfraConfigurations {
 	return EnvInfraConfigurations{Client: c}
 }
-
 func (c *Client) PipelineInfraConfigurations() PipelineInfraConfigurations {
 	return PipelineInfraConfigurations{Client: c}
 }
-
 func (c *Client) EnvRuns() EnvRuns {
 	return EnvRuns{Client: c}
 }
-
 func (c *Client) Providers() Providers {
 	return Providers{Client: c}
 }
-
 func (c *Client) ProviderCredentials() ProviderCredentials {
 	return ProviderCredentials{Client: c}
 }
-
 func (c *Client) Blocks() Blocks {
 	return Blocks{Client: c}
 }
-
 func (c *Client) BlockSyncs() BlockSyncs {
 	return BlockSyncs{Client: c}
 }
-
 func (c *Client) CapabilityCopies() CapabilityCopies {
 	return CapabilityCopies{Client: c}
 }
-
 func (c *Client) PipelineBlockSyncs() PipelineBlockSyncs {
 	return PipelineBlockSyncs{Client: c}
 }
-
 func (c *Client) Apps() Apps {
 	return Apps{Client: c}
 }
-
 func (c *Client) AppCapabilities() AppCapabilities {
 	return AppCapabilities{Client: c}
 }
-
 func (c *Client) AppPipelineCapabilities() AppPipelineCapabilities {
 	return AppPipelineCapabilities{Client: c}
 }
-
 func (c *Client) AppEnvs() AppEnvs {
 	return AppEnvs{Client: c}
 }
-
 func (c *Client) EnvVariables() EnvVariables {
 	return EnvVariables{Client: c}
 }
-
-func (c *Client) Builds() Builds { return Builds{Client: c} }
-
+func (c *Client) Builds() Builds       { return Builds{Client: c} }
 func (c *Client) BuildLogs() BuildLogs { return BuildLogs{Client: c} }
-
 func (c *Client) Deploys() Deploys {
 	return Deploys{Client: c}
 }
-
 func (c *Client) DeployLogs() DeployLogs {
 	return DeployLogs{Client: c}
 }
-
 func (c *Client) Workspaces() Workspaces {
 	return Workspaces{Client: c}
 }
-
 func (c *Client) WorkspaceDetails() WorkspaceDetails {
 	return WorkspaceDetails{Client: c}
 }
-
 func (c *Client) WorkspaceChanges() WorkspaceChanges {
 	return WorkspaceChanges{Client: c}
 }
-
 func (c *Client) WorkspaceModule() WorkspaceModule {
 	return WorkspaceModule{Client: c}
 }
-
 func (c *Client) WorkspaceConfigs() WorkspaceConfigs {
 	return WorkspaceConfigs{Client: c}
 }
-
 func (c *Client) WorkspaceOutputs() WorkspaceOutputs {
 	return WorkspaceOutputs{Client: c}
 }
-
 func (c *Client) WorkspaceVariables() WorkspaceVariables {
 	return WorkspaceVariables{Client: c}
 }
-
-func (c *Client) WorkspaceWorkflows() WorkspaceWorkflows {
-	return WorkspaceWorkflows{Client: c}
+func (c *Client) IntentWorkflows() IntentWorkflows {
+	return IntentWorkflows{Client: c}
 }
-
+func (c *Client) WorkspaceWorkflows() WorkspaceWorkflows { return WorkspaceWorkflows{Client: c} }
 func (c *Client) Runs() Runs {
 	return Runs{Client: c}
 }
-
 func (c *Client) RunConfigs() RunConfigs {
 	return RunConfigs{Client: c}
 }
-
 func (c *Client) PromotionConfigs() PromotionConfigs {
 	return PromotionConfigs{Client: c}
 }
-
 func (c *Client) RunLogs() RunLogs {
 	return RunLogs{Client: c}
 }
-
 func (c *Client) AutogenSubdomain() AutogenSubdomain {
 	return AutogenSubdomain{Client: c}
 }
-
 func (c *Client) AutogenSubdomainDelegation() AutogenSubdomainDelegation {
 	return AutogenSubdomainDelegation{Client: c}
 }
-
 func (c *Client) Domains() Domains {
 	return Domains{Client: c}
 }
-
 func (c *Client) Subdomains() Subdomains {
 	return Subdomains{Client: c}
 }
-
 func (c *Client) Modules() Modules {
 	return Modules{Client: c}
 }
-
 func (c *Client) ModuleVersions() ModuleVersions {
 	return ModuleVersions{Client: c}
 }
-
 func (c *Client) Events() Events {
 	return Events{Client: c}
 }
