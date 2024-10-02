b928d8d5812a4228c5a3e7e82d0cd6667842de0a
Brad Sickles <brad.sickles@gmail.com>
2024-10-02 16:51:25 -0400

    Added include_archived to workspace details Get

--- workspace_details.go
+++ workspace_details.go
@@ -6,6 +6,8 @@ import (
 	"gopkg.in/nullstone-io/go-api-client.v0/response"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 type WorkspaceDetails struct {
@@ -16,8 +18,11 @@ func (d WorkspaceDetails) basePath(stackId, blockId, envId int64) string {
 	return fmt.Sprintf("/orgs/%s/stacks/%d/blocks/%d/envs/%d/workspace_details", d.Client.Config.OrgName, stackId, blockId, envId)
 }
 
-func (d WorkspaceDetails) Get(ctx context.Context, stackId, blockId, envId int64) (*types.WorkspaceDetails, error) {
-	res, err := d.Client.Do(ctx, http.MethodGet, d.basePath(stackId, blockId, envId), nil, nil, nil)
+func (d WorkspaceDetails) Get(ctx context.Context, stackId, blockId, envId int64, includeArchived bool) (*types.WorkspaceDetails, error) {
+	q := url.Values{
+		"include_archived": []string{strconv.FormatBool(includeArchived)},
+	}
+	res, err := d.Client.Do(ctx, http.MethodGet, d.basePath(stackId, blockId, envId), q, nil, nil)
 	if err != nil {
 		return nil, err
 	}
