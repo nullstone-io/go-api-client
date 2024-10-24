2ff9148d17ab451692c39bdffca0e95c7bdd03a0
Brad Sickles <brad.sickles@gmail.com>
2024-10-24 13:14:51 -0400

    When diffing connection targets that have a nil env-id, fall back to env-name

---
+++ types/connection_target_test.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConnection_TargetEquals(t *testing.T) {
+	ptr := func(id int64) *int64 {
+		return &id
+	}
+
+	tests := map[string]struct {
+		a    *ConnectionTarget
+		b    *ConnectionTarget
+		want bool
+	}{
+		"a stack-id does not equal b stack-id": {
+			a:    &ConnectionTarget{StackId: 1, BlockName: "block"},
+			b:    &ConnectionTarget{StackId: 2, BlockName: "block"},
+			want: false,
+		},
+		"a env-id does not equal b env-id": {
+			a:    &ConnectionTarget{BlockName: "block", EnvId: ptr(1)},
+			b:    &ConnectionTarget{BlockName: "block", EnvId: ptr(2)},
+			want: false,
+		},
+		"a missing env-id equals b on env-name": {
+			a:    &ConnectionTarget{BlockName: "block", EnvId: nil, EnvName: "dev"},
+			b:    &ConnectionTarget{BlockName: "block", EnvId: ptr(2), EnvName: "dev"},
+			want: true,
+		},
+		"a does not equal missing b": {
+			a:    &ConnectionTarget{BlockId: 10, BlockName: "block"},
+			b:    nil,
+			want: false,
+		},
+		"missing a does not equal b": {
+			a:    nil,
+			b:    &ConnectionTarget{BlockId: 10, BlockName: "block"},
+			want: false,
+		},
+		"a equals b": {
+			a:    &ConnectionTarget{StackId: 1, StackName: "core", BlockId: 10, BlockName: "block", EnvId: ptr(110), EnvName: "dev"},
+			b:    &ConnectionTarget{StackId: 1, StackName: "core", BlockId: 10, BlockName: "block", EnvId: ptr(110), EnvName: "dev"},
+			want: true,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			got := (&Connection{Reference: test.a}).TargetEquals(Connection{Reference: test.b})
+			assert.Equal(t, test.want, got)
+		})
+	}
+}
