d3e3cc5ae0790bf2cb185321d64b77e2d3acc657
Brad Sickles <brad.sickles@gmail.com>
2024-10-25 08:43:53 -0400

    Added helper func to fill env in workspace config

--- types/workspace_config.go
+++ types/workspace_config.go
@@ -32,3 +32,41 @@ func (c WorkspaceConfig) Clone() (WorkspaceConfig, error) {
 	err := copier.CopyWithOption(&config, c, copier.Option{DeepCopy: true})
 	return config, err
 }
+
+func FillWorkspaceConfigMissingEnv(c *WorkspaceConfig, env Environment) {
+	envId := env.Id
+	fillRef := func(conn Connection) bool {
+		if conn.Reference == nil {
+			return false
+		}
+		filled := false
+		if conn.Reference.StackId == env.StackId {
+			if conn.Reference.EnvId == nil {
+				conn.Reference.EnvId = &envId
+				filled = true
+			}
+			if conn.Reference.EnvName == "" {
+				conn.Reference.EnvName = env.Name
+				filled = true
+			}
+		}
+		return filled
+	}
+	fillConns := func(conns Connections) bool {
+		filled := false
+		for name, conn := range conns {
+			if fillRef(conn) {
+				conns[name] = conn
+				filled = true
+			}
+		}
+		return filled
+	}
+
+	fillConns(c.Connections)
+	for i, capability := range c.Capabilities {
+		if fillConns(capability.Connections) {
+			c.Capabilities[i] = capability
+		}
+	}
+}
