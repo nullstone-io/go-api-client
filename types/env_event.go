eaa730319444d7996e874d6b946cf5eb80a9bcca
Brad Sickles <brad.sickles@gmail.com>
2024-10-28 14:53:15 -0400

    Added EnvEvent.OwningRepoUrl

--- types/env_event.go
+++ types/env_event.go
@@ -10,6 +10,9 @@ type EnvEvent struct {
 	// Name is used to uniquely identify an env event in an IaC file
 	// It is unique within an org/stack/env
 	Name string `json:"name"`
+	// OwningRepoUrl identifies the repo that configured this event
+	// If empty, then a user manually configured this event (and not via iac-sync)
+	OwningRepoUrl string `json:"owningRepoUrl"`
 	// Actions is a list of event actions (e.g. app-deployed)
 	// This event will only trigger on these event actions
 	Actions []EventAction `json:"actions"`
