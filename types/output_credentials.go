4186edf2c390bae4a7168f25ffad5b8dea7aaf7b
Brad Sickles <brad.sickles@gmail.com>
2024-11-23 10:20:56 -0500

    Explicit aws creds

--- types/output_credentials.go
+++ types/output_credentials.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 const (
 	ProviderAws   = "aws"
 	ProviderGcp   = "gcp"
@@ -18,6 +20,30 @@ type OutputCredentials struct {
 	// CredentialsType
 	CredentialsType string `json:"credentialsType"`
 
-	// Data contains the credentials information
+	// Aws contains aws credentials
+	Aws *OutputCredentialsAws `json:"aws,omitempty"`
+
+	// Data contains additional credential information
 	Data map[string]string `json:"data"`
+}
+
+type OutputCredentialsAws struct {
+	// AWS Access key ID
+	AccessKeyID string `json:"accessKeyID"`
+
+	// AWS Secret Access Key
+	SecretAccessKey string `json:"secretAccessKey"`
+
+	// AWS Session Token
+	SessionToken string `json:"sessionToken"`
+
+	// Source of the credentials
+	Source string `json:"source"`
+
+	// States if the credentials can expire or not.
+	CanExpire bool `json:"canExpire"`
+
+	// The time the credentials will expire at. Should be ignored if CanExpire
+	// is false.
+	Expires time.Time `json:"expires"`
 }
