18a1f6c87a164d506ccfef114c80e0a44eb4ec13
Brad Sickles <brad.sickles@gmail.com>
2024-11-23 10:46:46 -0500

    Added output-credentials api endpoint

---
+++ workspace_output_credentials.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"gopkg.in/nullstone-io/go-api-client.v0/response"
+	"gopkg.in/nullstone-io/go-api-client.v0/types"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type WorkspaceOutputCredentials struct {
+	Client *Client
+}
+
+func (c WorkspaceOutputCredentials) path(stackId int64, workspaceUid uuid.UUID) string {
+	return fmt.Sprintf("orgs/%s/stacks/%d/workspaces/%s/output-credentials", c.Client.Config.OrgName, stackId, workspaceUid)
+}
+
+// Create - POST /orgs/:orgName/stacks/:stackId/workspaces/:workspaceUid/output-credentials
+func (c WorkspaceOutputCredentials) Create(ctx context.Context, stackId int64, workspaceUid uuid.UUID, outputNames []string) (*types.OutputCredentials, error) {
+	q := url.Values{}
+	q.Set("output_names", strings.Join(outputNames, ","))
+	res, err := c.Client.Do(ctx, http.MethodPost, c.path(stackId, workspaceUid), q, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.ReadJsonPtr[types.OutputCredentials](res)
+}
