731f4ae25da5a141c034f7ddff8bea5cc245bfe6
Brad Sickles <brad.sickles@gmail.com>
2025-02-18 15:23:37 -0500

    Handle string for Connection.Target

--- types/connection.go
+++ types/connection.go
@@ -9,7 +9,7 @@ type Connection struct {
 
 	// Target refers to the ConnectionTarget that fulfills this connection
 	// This value is input by the user via UI or IaC and is not normalized
-	Target *ConnectionTarget `json:"target"`
+	Target *ConnectionTargetString `json:"target"`
 
 	// EffectiveTarget refers to the ConnectionTarget that fulfills this connection
 	// This value is a fully normalized, effective version of Target
@@ -29,7 +29,7 @@ type Connection struct {
 func (c *Connection) Equal(other Connection) bool {
 	return c.SchemaEquals(other) &&
 		c.Unused == other.Unused &&
-		isConnectionTargetEqual(c.Target, other.Target) &&
+		isConnectionTargetEqual(c.target(), other.target()) &&
 		isConnectionTargetEqual(c.EffectiveTarget, other.EffectiveTarget)
 }
 
@@ -45,6 +45,13 @@ func (c *Connection) SchemaEquals(other Connection) bool {
 }
 
 func (c *Connection) TargetEquals(other Connection) bool {
-	return isConnectionTargetEqual(c.Target, other.Target) &&
+	return isConnectionTargetEqual(c.target(), other.target()) &&
 		isConnectionTargetEqual(c.EffectiveTarget, other.EffectiveTarget)
+}
+
+func (c *Connection) target() *ConnectionTarget {
+	if c.Target == nil {
+		return nil
+	}
+	return &c.Target.ConnectionTarget
 }
