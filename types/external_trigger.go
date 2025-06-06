d6cc66516137db28361703555798bc2e63f6745d
Brad Sickles <brad.sickles@gmail.com>
2025-06-06 12:18:11 -0400

    Added support for "labeled" vcs pr event

--- types/external_trigger.go
+++ types/external_trigger.go
@@ -6,13 +6,14 @@ const (
 )
 
 const (
-	ExternalTriggerEventUser                 = "user"
-	ExternalTriggerEventAutomation           = "automation"
-	ExternalTriggerEventVcsPush              = "vcs-push"
-	ExternalTriggerEventVcsPullRequestOpened = "vcs-pull-request-opened"
-	ExternalTriggerEventGitopsEnable         = "gitops-enable"
-	ExternalTriggerEventGitopsChangeBranch   = "gitops-change-branch"
-	ExternalTriggerEventEnvCreate            = "env-create"
+	ExternalTriggerEventUser                  = "user"
+	ExternalTriggerEventAutomation            = "automation"
+	ExternalTriggerEventVcsPush               = "vcs-push"
+	ExternalTriggerEventVcsPullRequestOpened  = "vcs-pull-request-opened"
+	ExternalTriggerEventVcsPullRequestLabeled = "vcs-pull-request-labeled"
+	ExternalTriggerEventGitopsEnable          = "gitops-enable"
+	ExternalTriggerEventGitopsChangeBranch    = "gitops-change-branch"
+	ExternalTriggerEventEnvCreate             = "env-create"
 )
 
 // ExternalTrigger represents the source of an external trigger performing an action against Nullstone
@@ -21,7 +22,7 @@ type ExternalTrigger struct {
 	// Examples: ExternalTriggerSourceManual, ExternalTriggerSourceGithub
 	Source string `json:"source"`
 	// Event indicates what type of event caused the ExternalTrigger
-	// Examples: ExternalTriggerEventUser, ExternalTriggerEventVcsPush, ExternalTriggerEventVcsPullRequestOpened
+	// Examples: ExternalTriggerEventUser, ExternalTriggerEventVcsPush, ExternalTriggerEventVcsPullRequestOpened, ExternalTriggerEventVcsPullRequestLabeled
 	Event string `json:"event"`
 	// SourceUser indicates the user that caused the ExternalTrigger
 	// This user is dependent on the Source
@@ -29,6 +30,8 @@ type ExternalTrigger struct {
 	SourceUser ExternalTriggerUser `json:"sourceUser"`
 	// NullstoneUser is the user in Nullstone that is linked to SourceUser
 	NullstoneUser *User `json:"nullstoneUser"`
+	// Labels is a list of labels that were added during a "labeled" event
+	Labels []string `json:"labels"`
 }
 
 type ExternalTriggerUser struct {
