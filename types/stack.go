f84f8c71153e2e11cf2f47dcf01170b73a66fbb7
Brad Sickles <brad.sickles@gmail.com>
2025-09-04 08:50:09 -0400

    Added Stack.Status (#127)

--- types/stack.go
+++ types/stack.go
@@ -1,10 +1,18 @@
 package types
 
+type StackStatus string
+
+const (
+	StackStatusActive   = "active"
+	StackStatusArchived = "archived"
+)
+
 type Stack struct {
 	IdModel
-	Reference    string `json:"reference"`
-	Name         string `json:"name"`
-	OrgName      string `json:"orgName"`
-	Description  string `json:"description"`
-	ProviderType string `json:"providerType"`
+	Reference    string      `json:"reference"`
+	Name         string      `json:"name"`
+	OrgName      string      `json:"orgName"`
+	Description  string      `json:"description"`
+	ProviderType string      `json:"providerType"`
+	Status       StackStatus `json:"status"`
 }
