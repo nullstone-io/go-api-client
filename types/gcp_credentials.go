dacfa15c5d058aef74303ead2b6799fda04975de
Brad Sickles <brad.sickles@nullstone.io>
2025-12-09 09:18:01 -0500

    Added structs for gcp workload identity federation

--- types/gcp_credentials.go
+++ types/gcp_credentials.go
@@ -1,12 +1,16 @@
 package types
 
+import "fmt"
+
 const (
-	GcpAuthTypeServiceAccount = "serviceAccount"
+	GcpAuthTypeServiceAccount             = "serviceAccount"
+	GcpAuthTypeWorkloadIdentityFederation = "workloadIdentityFederation"
 )
 
 type GcpCredentials struct {
 	AuthType          string               `json:"authType"`
 	ServiceAccountKey GcpServiceAccountKey `json:"serviceAccount"`
+	WorkloadIdentity  GcpWorkloadIdentity  `json:"workloadIdentity"`
 }
 
 type GcpServiceAccountKey struct {
@@ -20,4 +24,18 @@ type GcpServiceAccountKey struct {
 	TokenUri                string `json:"token_uri"`
 	AuthProviderX509CertUrl string `json:"auth_provider_x509_cert_url"`
 	ClientX509CertUrl       string `json:"client_x509_cert_url"`
+}
+
+type GcpWorkloadIdentity struct {
+	ProjectNumber       string `json:"projectNumber"`
+	ProjectId           string `json:"projectId"`
+	ServiceAccountEmail string `json:"serviceAccountEmail"`
+
+	IdentityPoolId         string `json:"identityPoolId"`
+	IdentityPoolProviderId string `json:"identityPoolProviderId"`
+}
+
+// Audience should be a reference to the user's Workload Identity Pool Provider
+func (i GcpWorkloadIdentity) Audience() string {
+	return fmt.Sprintf("//iam.googleapis.com/projects/%s/locations/global/workloadIdentityPools/%s/providers/%s", i.ProjectNumber, i.IdentityPoolId, i.IdentityPoolProviderId)
 }
