8d63c3ebb5ef67105eae36cf3eb8424bf7a10086
Brad Sickles <brad.sickles@gmail.com>
2026-01-07 16:04:05 -0500

    Fixed changed update workspace module endpoint

--- workspace_module.go
+++ workspace_module.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	"gopkg.in/nullstone-io/go-api-client.v0/response"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
-	"net/http"
 )
 
 type WorkspaceModule struct {
@@ -14,10 +15,16 @@ type WorkspaceModule struct {
 }
 
 func (wmv WorkspaceModule) basePath(stackId, blockId, envId int64) string {
-	return fmt.Sprintf("/orgs/%s/stacks/%d/blocks/%d/envs/%d/module-version", wmv.Client.Config.OrgName, stackId, blockId, envId)
+	return fmt.Sprintf("/orgs/%s/stacks/%d/blocks/%d/envs/%d/module", wmv.Client.Config.OrgName, stackId, blockId, envId)
 }
 
-func (wmv WorkspaceModule) Update(ctx context.Context, stackId, blockId, envId int64, moduleInput types.WorkspaceModuleInput) error {
+type UpdateWorkspaceModuleInput struct {
+	Module        string                  `json:"module,omitempty"`
+	ModuleVersion string                  `json:"moduleVersion,omitempty"`
+	Connections   types.ConnectionTargets `json:"connections,omitempty"`
+}
+
+func (wmv WorkspaceModule) Update(ctx context.Context, stackId, blockId, envId int64, moduleInput UpdateWorkspaceModuleInput) error {
 	raw, _ := json.Marshal(moduleInput)
 	res, err := wmv.Client.Do(ctx, http.MethodPut, wmv.basePath(stackId, blockId, envId), nil, nil, json.RawMessage(raw))
 	if err != nil {
