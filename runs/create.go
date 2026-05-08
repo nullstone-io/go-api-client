4339fe6668f2f5b372da96fae6f71f6ff885f1f6
Brad Sickles <brad.sickles@gmail.com>
2026-05-07 20:20:26 -0400

    Added AppVersion to CreateRunInput (#161)

--- runs/create.go
+++ runs/create.go
@@ -9,13 +9,14 @@ import (
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 )
 
-func Create(ctx context.Context, cfg api.Config, workspace types.Workspace, commitSha string, isApproved *bool, latestUpdateAt time.Time, isDestroy bool, destroyDeps string) (*api.RunCreateResult, error) {
+func Create(ctx context.Context, cfg api.Config, workspace types.Workspace, commitSha string, isApproved *bool, latestUpdateAt time.Time, isDestroy bool, destroyDeps string, appVersion *string) (*api.RunCreateResult, error) {
 	input := api.CreateRunInput{
 		CommitSha:           commitSha,
 		IsDestroy:           isDestroy,
 		DestroyDependencies: destroyDeps,
 		IsApproved:          isApproved,
 		LatestUpdateAt:      latestUpdateAt,
+		AppVersion:          appVersion,
 	}
 
 	client := api.Client{Config: cfg}
